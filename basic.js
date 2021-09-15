13c5bf714501ef8febb03e1308e521ac566b20a4
ErikMoxley <exilerzero@yahoo.com>
2021-09-15 13:40:20 -0400

    onto the next module

---
+++ basic.js
@@ -0,0 +1,30 @@
+// ##basic alert
+// alert("hello from coding temple")
+
+// ##basic variables
+// var str1 = "Coding Temple";
+// var num1 = 5;
+// var str2 = str1 + num1;
+// alert(str2)
+
+//##creating and calling a function
+// function findTripTime (destinationA, destinationB){
+//     tripTime = destinationA + destinationB
+//     return tripTime
+// }
+
+// var dest1 = 10
+// var dest2 = 20
+
+// alert(findTripTime(dest1, dest2))
+
+// var dest3 = 300
+// var dest4 = 200
+
+// alert(findTripTime(dest3, dest4))
+
+// ##creating function and calling it
+// function displayAlert (){
+//     alert('testing alert')
+// }
+// displayAlert()
